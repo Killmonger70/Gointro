43e5855b2644aac44eab01dd349adaa7d2de2c21
Nicolas Ramirez <nico.elfo@gmail.com>
2021-05-21 09:26:45 -0500

    added short fmt description / descripcion corta de fmt añadida

--- introduction.go
+++ introduction.go
@@ -1,6 +1,7 @@
 // Nicolas Ramirez
 // First look at a normal go program design and creation
 // Primer vistazo al diseño y creacion de un programa Go 
+// fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
 
 package main
 
