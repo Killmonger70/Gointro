d37414a4ecb603ffdcf66f48b27ecaa4823bf58c
Nicolas Ramirez <nico.elfo@gmail.com>
2021-05-24 13:22:26 -0500

    New data type inside a variable / Nuevo tipo de datos dentro de una variable

--- variables.go
+++ variables.go
@@ -3,3 +3,12 @@
 // introduccion a las variables y los tipos de datos en golang
 
 package main
+
+import "fmt"
+
+var Nombre string = "Nico"
+var Edad int = 17
+
+func main() {
+	fmt.Println("hola , me llamo ", Nombre, " y tengo ", Edad, " años :)")
+}
