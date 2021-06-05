43bc6e97c02ddb6ba29a108cefd584163d253ba8
Nicolas Ramirez <nico.elfo@gmail.com>
2021-06-05 08:57:25 -0500

    difference between explicit and implicit variables in go / diferencia entre variables explicitas e implicitas en go

--- variables2.go
+++ variables2.go
@@ -1,12 +1,14 @@
 // Nicolas Ramirez
-// First look at a normal go program design and creation
-// Primer vistazo al diseño y creacion de un programa Go 
-// fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
+// different methos for defining variables
 
 package main
 
 import "fmt"
 
+var Name string ="Nico"; //EXPLICIT VARIABLES
+var Number = 1234; //implicit variables 
+
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Printf("%T", Number);
 }
