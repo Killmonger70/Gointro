77a71c64fa49876e8516464269f3e9a611baae7d
Nicolas Ramirez <nico.elfo@gmail.com>
2021-06-10 20:14:50 -0500

    clarification in one of the implicit variable declaration cases / clarificacion en uno de los casos de declaracion implicita de variables

--- variables2.go
+++ variables2.go
@@ -6,9 +6,13 @@ package main
 import "fmt"
 
 var Name string ="Nico"; //EXPLICIT VARIABLES
-var Number = 1234; //implicit variables 
+var Number = 1234; //implicit variables
+//Grado := 7; this type of declaration can only be used inside a function
 
 
 func main() {
+	Grado := 7;
+	fmt.Printf("%T", Name);
 	fmt.Printf("%T", Number);
+	fmt.Printf("%T", Grado);
 }
