6759e12e41660af4b6ea64cf238ca177ffe4305d
Nicolas Ramirez <nico.elfo@gmail.com>
2021-06-22 20:38:01 -0500

    Fmt has a lot of internal functionalities for a console related program / fmt tiene muchas funcionalidades internas para programas de consola

--- console1.go
+++ console1.go
@@ -1,10 +1,12 @@
 // Nicolas Ramirez
 // fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
+// fmt need two % for a ture percentage use
 
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Printf("Hello world %t", false); // decide if its t/f
+	fmt.Printf("hello world %b", 1025); // transform a number to a binary
 }
