80c50cfd96b63516d591c32a2c0d491f24ecedcb
Nicolas Ramirez <nico.elfo@gmail.com>
2021-06-22 20:53:03 -0500

    Hexadecimal interger utility / utilidad de enteros hexadecimales

--- console1.go
+++ console1.go
@@ -10,5 +10,5 @@ func main() {
 	fmt.Printf("Hello world %t", false); // decide if its t/f
 	fmt.Printf("hello world %b", 1025); // transform a decimal to a binary
 	fmt.Printf("hello world %o", 1025); // transform a decimal to an octal
-	fmt.Printf("hello world %o", 1025); // transform a decimal to an octal
+	fmt.Printf("hello world %x", 1025); // transform a decimal to a hexadecimal
 }
