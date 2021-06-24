823185626f44baff83506c411d59b82824493ccf
Nicolas Ramirez <nico.elfo@gmail.com>
2021-06-23 20:00:40 -0500

    New console related (input) archive / nuevo archivo de input consola

---
+++ console2.go
@@ -0,0 +1,12 @@
+// Nicolas Ramirez
+// First look at a normal go program design and creation
+// Primer vistazo al diseño y creacion de un programa Go 
+// fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
