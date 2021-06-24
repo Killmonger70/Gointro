4dfab17376c6263e0d60c854cc9a58a4d0246f9c
Nicolas Ramirez <nico.elfo@gmail.com>
2021-06-23 21:18:05 -0500

    Bufio scanner added / nuevo bufio escaner

--- console2.go
+++ console2.go
@@ -1,12 +1,18 @@
 // Nicolas Ramirez
-// First look at a normal go program design and creation
-// Primer vistazo al diseño y creacion de un programa Go 
 // fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+	//"strconv"
+	)
 
 func main() {
-	fmt.Println("Hello world")
+	scanner := bufio.NewScanner(os.Stdin);
+	scanner.Scan();
+	input := scanner.Text();
+	fmt.Printf("ingresaste: \n", input);
 }
