30674ccc7e4a75ed8548a1b8c257f8dcfb558598
Nicolas Ramirez <nico.elfo@gmail.com>
2021-07-09 21:45:43 -0500

    NEW CONCEPT OF ALIASES IN PACKAGE MANAGEMENT (EXAMPLE IN FILE)

---
+++ proyects/1.go
@@ -0,0 +1,12 @@
+// Nicolas Ramirez
+// fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
+// we are going to add the time package
+// se puede hacer un alias para el uso de los packetes importados
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
