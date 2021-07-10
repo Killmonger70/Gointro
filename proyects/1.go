11cefaaca24de7c8cde1c64126ee079f392eabbb
Nicolas Ramirez <nico.elfo@gmail.com>
2021-07-09 22:00:26 -0500

    New way to comment in a multiline format

--- proyects/1.go
+++ proyects/1.go
@@ -1,7 +1,10 @@
 // Nicolas Ramirez
-// fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
-// we are going to add the time package
-// se puede hacer un alias para el uso de los packetes importados
+// nueva manera de usar comentarios con /* */
+/*
+fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
+we are going to add the time package
+se puede hacer un alias para el uso de los packetes importados
+*/
 
 package main
 
