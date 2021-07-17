91746ff04d22025471874088eb2bea4edf46064e
Nicolas Ramirez <nico.elfo@gmail.com>
2021-07-16 21:17:00 -0500

    Added explication for the use of constant and an example

--- proyects/2.go
+++ proyects/2.go
@@ -1,12 +1,9 @@
-// Nicolas Ramirez
-// First look at a normal go program design and creation
-// Primer vistazo al diseño y creacion de un programa Go 
-// fmt permite usar utilidades de consola como println, similar a como lo hacen librerias de c++
+/* Nicolas Ramirez
+skull in ascii art */
 
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello world")
+
 }
---
+++ proyects/3.go
@@ -0,0 +1,11 @@
+// Nicolas Ramirez
+// const is used to create constant
+
+package main
+
+import "fmt"
+
+func main() {
+	const gravedad = 9.8 
+	fmt.Println(gravedad)
+}
