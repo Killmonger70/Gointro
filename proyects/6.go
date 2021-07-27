79476a2f95b253ce57ca9731750a1579dfe4951a
Nicolas Ramirez <nico.elfo@gmail.com>
2021-07-26 21:14:10 -0500

    NEW OPERAND TYPE FOR THE SPECIFIC PURPUSE OF MAKING ACUMULATOR TASK FASTER

--- proyects/6.go
+++ proyects/6.go
@@ -1,9 +1,21 @@
 /* Nicolas Ramirez
-skull in ascii art */
+there are diferent operator with the function of replacing the  var1 = var1 (insert operand) var2
+for example += -= /= *= */
 
 package main
 
 import "fmt"
 
+func main(){
+	var acumlator int32 = 25;
+	var num1 int32 = 3;
+	var divided int32 = 5;
+	text :=  "result is";
+
+	acumlator *= num1;
+	acumlator /= divided;
+
+
+	fmt.Println(text, acumlator);
 
 }
