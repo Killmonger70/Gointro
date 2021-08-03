77541274d70c0c223f4f1f482e849088b1693eee
Nicolas Ramirez <nico.elfo@gmail.com>
2021-08-02 21:07:46 -0500

    First design in the comicbook store software saga

--- proyects/7.go
+++ proyects/7.go
@@ -1,9 +1,27 @@
 /* Nicolas Ramirez
-skull in ascii art */
+comic book store management software proyects */
 
 package main
 
 import "fmt"
 
+func main(){
+	
+	// we are going to declare different variables that would be used in a comic book categorization
+	var  age, pageNumber, cost int32;
+	var title, author, artist, publisher string;
+	var grade float32;
+
+	title = "100 AÑOS DE SOLEDAD";
+	author = "Gabriel Garcia Marquez";
+	artist = "NONE";
+	publisher = "NORMA";
+	age = 1982;
+	pageNumber = 1000;
+	grade = 10.0;
+
+	fmt.Println(title, "by ", author, "drawn by ", artist, "published by ", publisher)
+	fmt.Println("Year", age, "pages ",pageNumber,  "rated ", grade)
+	fmt.Println("price", cost)
 
 }
