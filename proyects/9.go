59632adee82e1f578cb36854589dec7fce98fee5
Nicolas Ramirez <nico.elfo@gmail.com>
2021-08-19 21:33:44 -0500

    %f fmt verb used and described

--- proyects/9.go
+++ proyects/9.go
@@ -1,5 +1,5 @@
 /* Nicolas Ramirez
-more fmt utilities like %v %T  %d 
+more fmt utilities like %v %T %f 
 */
 
 package main
@@ -10,6 +10,8 @@ func main(){
 	// %T is used so the compiler tells us what data type a variable is
 	numeros := 1234;
 	letras :=  "abcd";
+	// %f is used so the compiler aproximates the number of decimals in a float
+	aprox := 12.03445;
 	fmt.Printf("the data types are %T and %T", numeros, letras)
-
+	fmt.Printf("aprox by .2 is %.2f", aprox) 
 }
