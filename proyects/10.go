e6c6f1e5bc4740728808d4af4d3b0842a00819da
Nicolas Ramirez <nico.elfo@gmail.com>
2021-08-30 21:16:01 -0500

    Added a new fmt functionality, Sprintln

---
+++ proyects/10.go
@@ -0,0 +1,16 @@
+/* Nicolas Ramirez
+skull in ascii art */
+
+package main
+
+import "fmt"
+
+func main(){
+	var string1 string = "hello";
+	string2 := "world";
+	salute := fmt.Sprintln(string1, string2);
+
+	fmt.Println(salute);
+
+
+}
