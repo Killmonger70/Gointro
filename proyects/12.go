a80f13ee69553d6f14afd61c5e642df39bf9bf84
Nicolas Ramirez <nico.elfo@gmail.com>
2021-09-02 21:14:04 -0500

    if statement example

--- proyects/12.go
+++ proyects/12.go
@@ -1,20 +1,12 @@
-/* 
-User input with scan in the fmt package
-In this case we can only recieve the first ection of the answer, it will recieve everything until the user 
-input a space character
+/* Nicolas Ramirez
+conditional statements in golang
 */
-
 
 package main
 
 import "fmt"
-
+ 
 func main(){
-	var Input string;
-	fmt.Println("Tell me something(use only one word):");
-	//scan expect an adress when we use it, thats why we need the & char before a variable
-	fmt.Scan(&Input);
-	output := fmt.Sprintf("did you say: %v. ?", Input);
-	fmt.Println(output);
+
 
 }
