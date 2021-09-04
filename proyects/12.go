db6bc2f3692cd189a78332f8dfacd0a0342f37a1
Nicolas Ramirez <nico.elfo@gmail.com>
2021-09-03 20:59:05 -0500

    If and Else Statements in golang

--- proyects/12.go
+++ proyects/12.go
@@ -7,6 +7,12 @@ package main
 import "fmt"
  
 func main(){
-
-
+	var var1 int = 2;
+	var var2 int = 3;
+	var var3 int = var2 + var1;
+	if  (var3 == 5){
+		fmt.Println("its 5");
+	} else {
+		fmt.Println("it aint 5 chief");
+	}
 }
