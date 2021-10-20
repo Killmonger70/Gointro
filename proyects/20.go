2228fd6ff161a461b0bdbf3a89acfa0d64ba4b69
Nicolas Ramirez <killmonger70@LAPTOP-VKETL3SS.localdomain>
2021-10-19 21:26:09 -0500

    Added description in the file for the functions syntax

--- proyects/20.go
+++ proyects/20.go
@@ -6,14 +6,14 @@ package main
 
 import "fmt"
 
-func Calcular(entrada int32) int32{
+func Calcular(entrada int32) int32{ //first, we declare in brackets the variables and its data types that we are going to use for the code to be executed; then, we declare the data type to be returne
 	var z int32 = 0;
 	z = entrada * 5;
-	return z;
+	return z;                  //this keyword is for the data that will be returned
 }
 
 func main(){
 	var x int32 = 8;
-	y := Calcular(x);
-	fmt.Println(y);
+	y := Calcular(x);          //We call the function, Then we use a data as an argument for the function inside the brackets
+	fmt.Println(y);        
 }
