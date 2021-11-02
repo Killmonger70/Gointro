eddd64b43033346c2308eaaafda3d1c45dce5da9
Nicolas Ramirez <killmonger70@LAPTOP-VKETL3SS.localdomain>
2021-11-01 21:04:51 -0500

    Defer para la concurrencia en golang

--- proyects/22.go
+++ proyects/22.go
@@ -1,11 +1,29 @@
 /* Nicolas Ramirez
-skull in ascii art */
+Defer in golang
+when we want to call a function at the end of the execution of another funtion, we use the keyword defer
+*/
 
 package main
 
 import "fmt"
 
+func calcularimpuestos(x int) int{
+	defer calculado();
+	verificar();
+	x = (x * 20)/100;
+	return x;
+}
+
+func verificar(){
+	fmt.Println("verificando impuestos");
+}
+
+func calculado(){
+	fmt.Println("impuestos calculados");
+}
+
 func main(){
-
-
+	var imptuestos int = 32;
+	resultado := calcularimpuestos(imptuestos);
+	fmt.Println(resultado)
 }
