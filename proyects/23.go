daf05197e27e7cdf798aa21a2ef5f34def0ae6ca
Nicolas Ramirez <killmonger70@LAPTOP-VKETL3SS.localdomain>
2021-11-02 21:37:17 -0500

    Example with: function, conditionals, defer.

--- proyects/23.go
+++ proyects/23.go
@@ -1,11 +1,42 @@
 /* Nicolas Ramirez
-skull in ascii art */
+Big example
+*/
+
 
 package main
 
 import "fmt"
 
+func calculando(){
+	fmt.Println("Calculando Resultado");
+}
+
+func calculado(){
+	fmt.Println("Resultado Calculado");
+}
+
+func calcular(opcion int, lado int) int{
+	if opcion==1{
+		calculando();
+		defer calculado();
+		lado = lado * 4;
+		return lado;
+	} else {
+		calculando();
+		defer calculado();
+		lado = lado * lado;
+		return lado;
+	}
+}
+
+
 func main(){
-
-
+	opcion := 0;
+	fmt.Println("1=Perimetro, otro numero=area");
+	fmt.Scanln(&opcion);
+	entrada := 0;
+	fmt.Println("ingrese lado");
+	fmt.Scanln(&entrada);
+	resultado := calcular(opcion, entrada);
+	fmt.Println(resultado);
 }
