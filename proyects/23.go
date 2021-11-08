921b4df6585111b3f435e85165ba81c13372e4a5
Nicolas Ramirez <killmonger70@LAPTOP-VKETL3SS.localdomain>
2021-11-07 20:34:53 -0500

    Function that calculates the fuel depending on the planet

--- proyects/23.go
+++ proyects/23.go
@@ -10,6 +10,20 @@ func CombustibleRestante(fuel int) {
 	fmt.Println("You have", fuel, "Gallons of fuel remaining");
 }
 
+func calculateFuel(planet string) int{
+	var fuel int;
+	switch planet{
+	case "Venus":
+		fuel = 300000;
+	case "Mercury":
+		fuel = 500000;
+	case "Mars":
+		fuel = 700000;
+	default:
+		fuel = 0;
+	}
+	return fuel;
+}
 
 func main(){
 	
